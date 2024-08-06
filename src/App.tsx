f2c5bc542b0d856335534044759e6fba1b01da95
JiwonShin <dev.jiwonshin@gmail.com>
2024-08-06 15:59:08 +0900

    tsconfig.json에 백엔드의 data관련 코드들과 연결하는 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-import outputs from "./amplify_outputs.json";
+import outputs from "./amplify_outputs.json"; // 야매. 원래는 ../amplify_outputs.json 임.
 
 Amplify.configure(outputs);
 
@@ -17,29 +17,3 @@ export default function App() {
     </Authenticator>
   );
 }
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
